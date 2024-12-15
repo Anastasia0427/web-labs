88fce8cc6f422d3d1a3490b3f8cefc975ea37bf3
Anastasia <167350995+Anastasia0427@users.noreply.github.com>
2024-12-15 16:25:09 +0300

    Add files via upload

---
+++ lab1/lab1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#вывести в текстовый файл все объекты корневой директории, удовлетворяющие правам доступа 755
+
+output_file="objects.txt"
+
+find / -maxdepth 1 -perm 755 -print > "$output_file" 2> /dev/null
+echo "List of objects written to file $output_file"
